0f89c46de86e61c6a89850a55037908c936034c5
kristinashch <47273169+KrisTinaShch@users.noreply.github.com>
2024-01-24 15:35:20 +0200

    fix branch changes

--- schedules/script.js
+++ schedules/script.js
@@ -1,18 +1,22 @@
+const { iata, api_key } = getQueryVariables();
+const arrivalsLink = `https://airlabs.co/api/v9/schedules?arr_iata=${iata}&api_key=${api_key}`;
+const departuresLink = `https://airlabs.co/api/v9/schedules?dep_iata=${iata}&api_key=${api_key}`;
+const currentTime = new Date();
+const currentTimeUTC = new Date(currentTime.getTime() + currentTime.getTimezoneOffset() * 60000)
 
-
-// разбивает URL и ищет IATA и API-KEY
 function getQueryVariables() {
-  var query = window.location.search.substring(1);
-  var vars = query.split('&');
-  var result = {
+  let query = window.location.search.substring(1);
+  let vars = query.split('&');
+  let result = {
     iata: 'SIN',
     api_key: 'SERGEY-STAM-KEY'
   };
 
-  for (var i = 0; i < vars.length; i++) {
-    var pair = vars[i].split('=');
-    var decodedName = decodeURIComponent(pair[0]);
-    var decodedValue = decodeURIComponent(pair[1]);
+  for (let i = 0; i < vars.length; i++) {
+    let pair = vars[i].split('=');
+    let decodedName = decodeURIComponent(pair[0]);
+    let decodedValue = decodeURIComponent(pair[1]);
+    decodedValue = decodedValue.replace(/["']/g, "");
 
     if (decodedName === 'iata') {
       result.iata = decodedValue;
@@ -20,162 +24,244 @@ function getQueryVariables() {
       result.api_key = decodedValue;
     }
   }
-
   return result;
 }
 
+const fetchData = async () => {
+  const resultArrivals = await fetch(arrivalsLink);
+  const dataArrivals = await resultArrivals.json();
+  await createAccordionItems(dataArrivals, 0, "Arrivals");
 
-// Форматирование даты 
-function formatTime(date_string) {
-  const date_string_date = new Date(date_string);
-  const date_string_hours = date_string_date.getHours();
-  const date_string_minutes = date_string_date.getMinutes();
-  console.log();
-  return date_string_hours.toString().padStart(2, '0') + ':' + date_string_minutes.toString().padStart(2, '0');
+};
+const fetchDataDepartures = async () => {
+  const resultdDepartures = await fetch(departuresLink);
+  const dataDepartures = await resultdDepartures.json();
+  await createAccordionItems(dataDepartures, 0, "Departures");
 }
 
-function formatTimePMandAM(date_string) {
-  const date_string_date = new Date(date_string);
-  return `${date_string_date.toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true })}`
-}
-// Форматирование даты 
-function formatDate(date_string) {
-  const monthsShort = [
-    'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'
-  ];
 
-  const arr_date = new Date(date_string);
-  const arr_date_day = arr_date.getDate();
-  const arr_date_month = monthsShort[arr_date.getMonth()];
-  return `${arr_date_day} ${arr_date_month}`;
-}
+async function getAirportName(iata) {
+  const response = await fetch(`https://airlabs.co/api/v9/airports?iata_code=${iata}&api_key=${api_key}`);
+  const data = await response.json();
 
-// Проверка даты 
-function checkArrivalTime(estimated_time, exact_time) {
-  return typeof estimated_time === "undefined" ? exact_time : `${formatTime(estimated_time)}`;
-}
-
-function fetchAirportData(iata_code, callback) {
-  let linkParameters = getQueryVariables("iata", "api_key");
-
-  const cachedAirportData = localStorage.getItem(`airport_${iata_code}`);
-  if (cachedAirportData) {
-    callback(JSON.parse(cachedAirportData));
-    return;
+  if (data.response && data.response.length > 0) {
+    const airportData = {
+      iata: data.response[0].iata_code,
+      city: data.response[0].city,
+      airport_name: data.response[0].name,
+    };
+    localStorage.setItem(`airport_${iata}`, JSON.stringify(airportData));
+    return airportData;
+  } else {
+    return null;
   }
-
-  fetch(`https://airlabs.co/api/v9/airports?iata_code=${iata_code}&api_key=${linkParameters.api_key}`)
-    .then(response => response.json())
-    .then(data => {
-      if (data && Array.isArray(data.response) && data.response.length > 0) {
-        const airportData = {
-          iata: data.response[0].iata_code,
-          city: data.response[0].city,
-          airport_name: data.response[0].name
-        };
-
-        localStorage.setItem(`airport_${iata_code}`, JSON.stringify(airportData));
-
-        callback(airportData);
-      } else {
-        console.error('Error');
-        throw new Error('Airport data not found');
-      }
-    })
-    .catch(error => {
-      console.error('Error:', error);
-      throw error;
-    });
 }
 
-function fetchAndDisplayFlights() {
-  let linkParameters = getQueryVariables("iata", "api_key");
+async function createAccordionItems(data, counter = "0", accordionType) {
+  // create accordion items
+  const accordion = document.querySelector(`.accordion${accordionType}`);
+  const fragment = document.createDocumentFragment();
+  for (const item of data.response) {
+    counter++;
+    const itemMarkup = await generateAccordionItemMarkup(item, counter, accordionType);
+    const tempDiv = document.createElement('div');
+    tempDiv.innerHTML = itemMarkup;
+    fragment.appendChild(tempDiv.firstElementChild);
 
-  fetch(`https://airlabs.co/api/v9/schedules?arr_iata=${linkParameters.iata}&api_key=${linkParameters.api_key}`)
-    .then(response => response.json())
-    .then(data => {
-      if (data && Array.isArray(data.response)) {
-        const flightList = document.querySelector('.schedules-list');
-        let accordionIdCounter = 0;
+  }
+  accordion.appendChild(fragment);
 
-        flightList.innerHTML = '';
-        data.response.forEach((flight) => {
-          fetchAirportData(flight.dep_iata, (depAirportData) => {
-            accordionIdCounter++;
-            const flightListItem = document.createElement('div');
-            flightListItem.className = 'accordion-item';
-            flightListItem.innerHTML = `
-              <h2 class="accordion-header" id="panelsStayOpen-heading${accordionIdCounter}">
-                <button class="accordion-button" type="button" data-bs-toggle="collapse"
-                  data-bs-target="#panelsStayOpen-collapse${accordionIdCounter}" aria-expanded="true" aria-controls="panelsStayOpen-collapse${accordionIdCounter}">
-  
-                  <span class="flight-time">
-                    ${checkArrivalTime(flight.arr_estimated, flight.arr_time)}
-                  </span>
-  
-                  <span class="flight-number">${flight.airline_iata}  ${flight.flight_number}</span>
-                  <span class="flight-status">${flight.status}</span>
-                  <span class="flight-iata">${depAirportData.city}
-                    <span class="flight-dep-iata">${flight.dep_iata}</span>
-                  </span>
-                </button>
-              </h2>
-              <div id="panelsStayOpen-collapse${accordionIdCounter}" class="accordion-collapse collapse "
-                aria-labelledby="panelsStayOpen-heading${accordionIdCounter}">
-                <div class="accordion-body">
-                  <div class="accordion-body-header">
-                    <div class="row">
-                      <div class="col-md-3">
-                        <p class="flight-airport-name fw-bold">${depAirportData.airport_name}</p>
-                        <p class="flight-start-town fw-bold">${depAirportData.city}</p>
-                        <p class="flight-date text-secondary">${formatDate(flight.arr_time)}</p>
-                        <p class="flight-time mt-1">${formatTimePMandAM(flight.arr_time)}</p>
-                        <p class="flight-status small ">5:10 AM</p>
-                      </div>
-                      <div class="col-md-6">
-  
-                      </div>
-                      <div class="col-md-3 text-end">
-                        <p class="flight-airport-name fw-bold">DXB Istanbul New Airport</p>
-                        <p class="flight-start-town fw-bold">Istanbul</p>
-                        <p class="flight-date text-secondary">16 oct</p>
-                        <p class="flight-time mt-1">5:10 AM</p>
-                        <p class="flight-status small ">5:10 AM</p>
-                      </div>
-                    </div>
-                  </div>
-                  <div class="accordion-body-footer">
-  
-                  </div>
-                </div>
-              </div>
-            `;
-            flightList.appendChild(flightListItem);
-          });
-        });
-      } else {
-        console.error('Error');
-      }
-    })
-    .catch(error => console.error('Error:', error));
-}
-
-document.addEventListener('DOMContentLoaded', function () {
-  fetchAndDisplayFlights();
-
-  document.querySelector('.schedules-list').addEventListener('click', function (event) {
-    if (event.target && event.target.matches('.accordion-button')) {
-      const accordionItem = event.target.closest('.accordion-item');
-
-      if (accordionItem) {
-        if (!event.target.classList.contains('collapsed')) {
-          accordionItem.classList.add('accordion-box-shadow');
-        } else {
-          accordionItem.classList.remove('accordion-box-shadow');
-        }
-      }
+  // change the appearance of the accordion on the mobile 
+  const accordionItems = document.querySelectorAll('.accordion-collapse');
+  accordionItems.forEach((item) => {
+    if (window.innerWidth < 768) {
+      item.classList.add('show');
+    } else {
+      item.classList.remove('show');
     }
   });
+
+  // add accordion item shadow 
+  const accordionButtons = document.querySelectorAll('.accordion-button');
+  const accordionItem = document.querySelectorAll('.accordion-item');
+  accordionButtons.forEach((button, index) => {
+    accordionButtons.forEach((button, index) => {
+      button.addEventListener("click", () => {
+        setTimeout(() => {
+          if (!button.classList.contains('collapsed')) {
+            accordionItem[index].classList.add('accordion-box-shadow');
+          } else {
+            accordionItem[index].classList.remove('accordion-box-shadow');
+          }
+        }, 0);
+      });
+    });
+  });
+}
+
+
+
+async function generateAccordionItemMarkup(item, counter, accordionType) {
+
+  const airportDataDep = await getAirportName(item.dep_iata);
+  const airportArrivalsInfo = JSON.parse(localStorage.getItem(`airport_${iata}`)) || await getAirportName(item.arr_iata);
+
+  const flightInfo = {
+    status: item.status,
+    flightNumber: `${item.airline_iata || ' '} ${item.flight_number || ' '}`,
+    flightDuration: item.duration || '-',
+    airlineIata: item.airline_iata || ' ',
+    // departures
+    departureIata: item.dep_iata || '-',
+    departureTime: item.dep_time || '-',
+    departureEstimated: item.dep_estimated|| ' - ',
+    departureUTCTime: item.dep_time_utc|| ' - ',
+    departureEstimatedUTCTime: item.dep_estimated_utc|| ' - ',
+    departureTerminal: item.dep_terminal|| ' - ',
+    departureGate: item.dep_gate|| ' - ',
+    // arrivals
+    arrivalsIata: item.arr_iata || '-',
+    arrivalsTime: item.arr_time || '-',
+    arrivalsEstimated: item.arr_estimated || '-',
+    arrivalsUTCTime: item.arr_time_utc || '-',
+    arrivalsEstimatedUTCTime: item.arr_estimated_utc || '-',
+    arrivalsTerminal: item.arr_terminal || '-',
+    arrivalsGate: item.gate || '-',
+    arrivalsBaggage: item.arr_baggage || '-',
+    // methods
+    getCurrentFlightTime() {
+      const actualFlightUTCDeparture = getActualFlightTime(flightInfo.departureUTCTime, flightInfo.departureEstimatedUTCTime);
+      const actualFlightUTCArrivial = getActualFlightTime(flightInfo.arrivalsUTCTime, flightInfo.arrivalsEstimatedUTCTime);
+      const depTest = new Date(actualFlightUTCDeparture);
+      const arrTest = new Date(actualFlightUTCArrivial);
+      const totalDifference = (arrTest - depTest) / (1000 * 60); // in minutes
+      const restTimeFlight = Math.round((arrTest - currentTimeUTC) / (1000 * 60)); // in minutes
+      if (currentTimeUTC > depTest && currentTimeUTC < arrTest) {
+        const restTimeFlightInPercent = Math.round((restTimeFlight * 100) / (totalDifference));
+        return `left:${83 - restTimeFlightInPercent}%`;
+      }
+    },
+  }
+  flightInfo.getCurrentFlightTime();
+  return `
+        <div class="accordion-item px-0 px-sm-3 py-3">
+    <h2 class="accordion-header d-none d-md-block " id="panelsStayOpen${accordionType}-heading${counter}">
+        <button class="accordion-button p-0 py-3 " type="button" data-bs-toggle="collapse"
+            data-bs-target="#panelsStayOpen${accordionType}-collapse${counter}" aria-expanded="true"
+            aria-controls="panelsStayOpen${accordionType}-collapse${counter}">
+            <span class="fw-normal">${formatTime(flightInfo.departureTime)}</span>
+            <span class="blue-font">${flightInfo.flightNumber }</span>
+            <span class="flight-status d-none d-md-block rounded fw-bold text-center ${flightInfo.status }">${flightInfo.status }</span>
+            <span>${airportArrivalsInfo.city } <span class="airport-code">${flightInfo.departureIata }</span> </span>
+        </button>
+    </h2>
+    <div id="panelsStayOpen${accordionType}-collapse${counter}" class="accordion-collapse collapse"
+        aria-labelledby="panelsStayOpen${accordionType}-heading${counter}">
+        <div class="accordion-body p-sm-0 p-2">
+            <div class="body-innerline">
+                <div class="container-fluid">
+                    <div class="row justify-content-between py-3 px-2 border rounded mb-0 position-relative align-items-center row-gap-0">
+                        <div class="col-md-2 col-3 p-0">
+                            <div class="airport-name d-none d-md-block fw-bold">
+                                ${airportDataDep.airport_name }
+                                <span class="airport-code">${flightInfo.departureIata }</span>
+                            </div>
+                            <div class="city-name fw-bold">${airportDataDep.city }</div>
+                            <div class="date text-muted">${formatDate(flightInfo.departureTime)}</div>
+                            <div class="time fw-bold">${checkEstimatedTime(item.dep_estimated, item.dep_time)}</div>
+                            <div class="country-code d-block d-md-none blue-iata-mobile ">${flightInfo.departureIata }</div>
+                        </div>
+                        <div class="col-md-8 col-6 mx-md-auto d-flex flex-column align-items-center gap-1 p-0 justify-content-center">
+                            <div class="flight-flag mb-2">
+                                <img src="${flightInfo.airlineIata ? `https://airlabs.co/img/airline/m/${flightInfo.airlineIata}.png` : "../images/placeholder.png"}" alt="" class="rounded-circle">
+                            </div>
+                            <div class="flight-line position-relative">
+                                <img src="../images/plane-trip.svg" alt="" class="plane-image ${flightInfo.status}" style="${flightInfo.status == 'active' ? flightInfo.getCurrentFlightTime() : ''}">
+                            </div>
+                            <div class="text-muted mt-2 flight-info">${toHoursAndMinutes(flightInfo.flightDuration)}</div>
+                            <div class="text-muted flight-info">${flightInfo.flightNumber }</div>
+                        </div>
+                        <div class="col-md-2 col-3 p-0 text-end ">
+                            <div class="airport-name d-none d-md-block fw-bold">${airportArrivalsInfo.airport_name }
+                                <span class="airport-code">${flightInfo.arrivalsIata }</span></div>
+                            <div class="city-name fw-bold">${airportArrivalsInfo.city }</div>
+                            <div class="date text-muted">${formatDate(flightInfo.arrivalsTime)}</div>
+                            <div class="time fw-bold"> ${checkEstimatedTime(item.arr_estimated, item.arr_time)}</div>
+                            <div class="flight-status rounded fw-bold text-center d-block d-md-none ${flightInfo.status }">
+                                ${flightInfo.status }</div>
+                            <div class="country-code d-block d-md-none blue-iata-mobile float-end">${flightInfo.arrivalsIata } </div>
+                        </div>
+                        <div class="col-md-12 mt-4 d-none d-md-block p-0 pb-3">
+                            <div class="d-flex gap-5 justify-content-between">
+                                <div class="d-flex gap-3 border rounded">
+                                    <div class="p-2 pe-4 border-end">
+                                        <p class="text-muted">Terminal</p>
+                                        <p class="fw-bold">${flightInfo.departureTerminal }</p>
+                                    </div>
+                                    <div class="p-2 pe-4">
+                                        <p class="text-muted">Gate</p>
+                                        <p class="fw-bold">${flightInfo.departureGate }</p>
+                                    </div>
+                                </div>
+
+                                <div class="d-flex gap-3 border rounded">
+                                    <div class="p-2 pe-4 border-end">
+                                        <p class="text-muted">Terminal</p>
+                                        <p class="fw-bold">${flightInfo.arrivalsTerminal }</p>
+                                    </div>
+                                    <div class="p-2 pe-4 border-end">
+                                        <p class="text-muted">Gate</p>
+                                        <p class="fw-bold">${flightInfo.arrivalsGate }</p>
+                                    </div>
+                                    <div class="p-2 pe-4">
+                                        <p class="text-muted">Baggage Claim</p>
+                                        <p class="fw-bold">${flightInfo.arrivalsBaggage }</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+  `
+}
+function getActualFlightTime(time, estimatedTime) {
+  let flightTime;
+  return estimatedTime ? flightTime = estimatedTime : flightTime = time;
+}
+function getActiveFlightPostion(depUTCTime) {
+  // console.log(depUTCTime);
+}
+
+// Change time format to AM/PM
+function formatTime(time_string) {
+  const time_string_format = new Date(time_string);
+  return `${time_string_format.toLocaleString('en-US', { timeStyle: "short" })}`
+}
+
+// Adding a month to a date
+function formatDate(date_string) {
+  const date_string_format = new Date(date_string);
+  return `${date_string_format.toLocaleString('en-US', { day: "numeric", month: "short" })}`;
+}
+
+function checkEstimatedTime(estimated_time, exact_time) {
+  return typeof estimated_time === "undefined" ? formatTime(exact_time) : `${formatTime(estimated_time)}<br><span class="flight-delay fw-normal text-danger text-decoration-line-through">${formatTime(exact_time)} </span>`;
+}
+
+function toHoursAndMinutes(totalMinutes) {
+  const hours = Math.floor(totalMinutes / 60);
+  const minutes = totalMinutes % 60;
+  return `${hours} hr ${minutes > 0 ? ` ${minutes} min` : ''}`;
+}
+
+
+document.addEventListener('DOMContentLoaded', (event) => {
+  fetchData();
+  fetchDataDepartures();
 });
 
 
